8972daec6737a2692bfaa3e0df33c1b90b1b60b8
Doug Mellon <dm@dougmellon.com>
2020-11-27 22:13:49 -0700

    Added books to readings.js.

--- src/pages/readings.js
+++ src/pages/readings.js
@@ -11,7 +11,10 @@ const ReadingPage = () => {
             />
             <ul>
                 <li><a href="https://dataintensive.net">(current) Designing Data-Intensive Applications</a></li>
+                <li><a href="https://algs4.cs.princeton.edu/home/">(current) Algorithms</a></li>
+                <li><a href="https://github.com/getify/You-Dont-Know-JS">You Don't Know JS Yet</a></li>
                 <li><a href="https://itrevolution.com/the-phoenix-project/">The Phoenix Project</a></li>
+                <li><a href="https://horstmann.com/corejava/">Core Java: Volume 1</a></li>
             </ul>
         </>
     )
